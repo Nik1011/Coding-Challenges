365c184a9ff713ae9b8c08603ecefd9938bea0fb
unknown <niko.lai@outlook.de>
2023-06-06 11:08:44 +0200

    almost done

--- Fourier_Series/fourier.js
+++ Fourier_Series/fourier.js
@@ -11,19 +11,13 @@ const canvas = document.getElementById("canvas");
 const offset = 500;
 const ctx = canvas.getContext("2d");
 
-let centerX = 150;
-let centerY = 250;
-let radius = 100;
+
 let time = 0;
 let x;
 let y;
 let wave = [];
 
 
-
-function createCanvas(){
-    ctx.clearRect(0,0,500,500); 
-}
 
 function drawEllipse(x,y,radius){
     ctx.beginPath();
@@ -33,49 +27,60 @@ function drawEllipse(x,y,radius){
 
 
 function draw(){
-
-    let n = 1;
+    
     ctx.clearRect(0,0,1000,500);
     
+    let radius = 100;
+    let centerX = 150;
+    let centerY = 250;
     
-    x = centerX + Math.sin(time)* radius;
-    y = centerY + Math.cos(time)* radius;
-    drawEllipse(centerX, centerY, radius);
-    
-    
-    let s = drawEllipse(x,y,3);
-    ctx.fill();
-    time += 0.01;
-    if( time >= 360){
-        time = 0;
+    for(i = 1; i < 5; i++){
+        
+        let n = i * 2 + 1;
+        x = centerX + Math.sin(n * time) * radius;
+        y = centerY + Math.cos(n * time) * radius;
+        // 1st circle
+        drawEllipse(centerX, centerY, radius);
+        
+        // nth Circle
+        drawEllipse(x,y,3);
+        ctx.fill();
+        
+        centerX = x;
+        centerY = y;
+        radius = radius / 2;
+        
+        time += 0.001;
+        if( time >= 360){
+            time = 0;
+        }
+        
+        
+        // radiusline 
+        ctx.beginPath();
+        ctx.moveTo(centerX,centerY);
+        ctx.lineTo(x,y);
+        ctx.stroke();
+        
     }
-
+    wave.unshift(new Point(x,y));
+    
     // line to function
     ctx.beginPath();
     ctx.moveTo(x,y);
     ctx.lineTo(500,y);
     ctx.stroke();
-
-
-    // radiusline 
-    ctx.beginPath();
-    ctx.moveTo(centerX,centerY);
-    ctx.lineTo(x,y);
-    ctx.stroke();
-
-    drawEllipse(x,y, 15);
-    
-
-    wave.unshift(new Point(x,y));
-
+        
+    // draw function
     for(let i = 0; i < wave.length; i++){
         drawEllipse(i + offset, wave[i].y, 1);
         ctx.fill();
     }
-
-    if(wave.length > 400){
+    
+    if(wave.length > 350){
         wave.pop();
     }
+
 
     window.requestAnimationFrame(draw);
 }
